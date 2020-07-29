04d04ed3eda37847066782fd5d39aa2ca4cb23d2
parksale <psi0613@naver.com>
2020-07-29 15:00:50 +0900

    BOJ

--- 19535.cpp
+++ 19535.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long N, D, G;
-vector<long long> vt[300005];
+typedef long long ll;
+ll N, D, G;
+vector<ll> vt[300005];
+bool v[300005];
 void go(int n) {
-	for (int nn : vt[n]) {
-		D += (vt[n].size() - 1) * (vt[nn].size() - 1);
+	for (ll nn : vt[n]) {
+		if (v[nn]) continue;
+		ll v1 = vt[n].size() - 1, v2 = vt[nn].size() - 1;
+		D += v1 * v2;
+		v[nn] = 1;
 		go(nn);
 	}
 }
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+	cin.tie(0);
 	cin >> N;
-	for (int i = 1; i < N; ++i) {
-		int a, b;
+	for (int i = 1, a, b; i < N; ++i) {
 		cin >> a >> b;
 		vt[a].push_back(b);
 		vt[b].push_back(a);
 	}
 	for (int i = 1; i <= N; ++i) {
-		if (vt[i].size() >= 3) {
-			long long k = vt[i].size();
+		ll k = vt[i].size();
+		if (k >= 3) {
 			G += (k * (k - 1) * (k - 2)) / 6;
 		}
 	}
+	G *= 3;
+	v[1] = 1;
 	go(1);
-	if (D > 3 * G) {
+	if (D > G) {
 		cout << "D";
 	}
-	else if (D == 3 * G) {
+	else if (D == G) {
 		cout << "DUDUDUNGA";
 	}
 	else {
